6c9f6c81734a1317c6a066a432871b1e54bcbfc0
Atiqur Rahaman <atikrahman146@gmail.com>
2017-10-05 10:13:44 +0600

    Description demo

---
+++ AutoBoxing/AutoBoxing.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package autoboxing;
+
+/**
+ *
+ * @author J2EE-33
+ */
+public class AutoBoxing {
+    private double balance = 0.0;
+    private double income = 0.0;
+    private double expense = 0.0;
+    
+    public double addBalance(double income){
+        balance += income;
+        return balance;
+    }
+    
+    public double doExpense(double salary){
+        balance -= expense;
+        return balance;
+    }
+    
+}
